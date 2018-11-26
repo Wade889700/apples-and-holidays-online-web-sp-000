7bd01a3c2076c43fb60426b5b69006712468593a
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-26 01:46:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,10 +47,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash.each do |season, festival|
-    if season == :winter
-      return festival
-    end
+  holiday_hash[:winter].values.flatten
   end
 end
 
